9fa6bc8903a1340619fdef9fe523a6bf6fd7b951
Gabriel Cavalcanti <141736140+gabriel-cvt@users.noreply.github.com>
2026-05-23 12:02:11 -0300

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        memo = {}
+        n = len(nums) -1
+        def dp(index):
+            if index >= n:
+                return 0
+            
+            if index in memo:
+                return memo[index]
+            if nums[index] == 0:
+                memo[index] = float('inf')
+                return memo[index]
+
+            result = float('inf')
+            for i in range(nums[index], 0, -1):
+                result = min(result, dp(index + i) + 1)
+            memo[index] = result
+            return result
+        return dp(0)
